aa34a8f5903ce4cc9f69b52a9c46ab651a2125b1
Kate-Kitkat19 <katekateee1911@gmail.com>
2023-04-03 01:09:12 +0300

    dark/light theme

---
+++ src/redux/store.js
@@ -0,0 +1,35 @@
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { themeReducer } from './theme/themeSlice';
+
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+];
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+export const store = configureStore({
+  reducer: {
+    theme: persistReducer(persistConfig, themeReducer),
+  },
+  middleware,
+});
+
+export const persistor = persistStore(store);
