98dd293886375afb5bb60d4fc60ce8c127e7c0b0
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2023-04-02 22:07:44 +0300

    add basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {BrowserRouter} from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/so-yummy-front'>
+    <BrowserRouter basename='https://github.com/BondarViktoriia/so-yummy-front'>
         <App />
     </BrowserRouter>
   
