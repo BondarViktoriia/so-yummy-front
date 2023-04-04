418d4c84fb60fd536cfcf4c90a799f5d4a75a373
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2023-04-04 12:01:59 +0300

    change basename,prev doesnt work

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/BondarViktoriia/so-yummy-front">
+        <BrowserRouter basename="https://bondarviktoriia.github.io/so-yummy-front">
           <App />
         </BrowserRouter>
       </PersistGate>
