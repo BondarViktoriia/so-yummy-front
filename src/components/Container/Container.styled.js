ef2488827aff5afd299efc5b344b12249182c1ae
lady-araminta <jparkhomenko@gmail.com>
2023-04-04 21:29:16 +0300

    container

---
+++ src/components/Container/Container.styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const ContainerStyled = styled.div`
+outline 1px solid red;
+width: 375px;
+margin-left: auto;
+margin-right: auto;
+@media (min-width: 768px) {
+    width: 768px;
+}
+@media (min-width: 1440px) {
+    width: 1440px;
+}
+`;
