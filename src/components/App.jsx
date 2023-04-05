ff83a9f432e306c486b09b47dc65bc186258c2d8
Kate-Kitkat19 <katekateee1911@gmail.com>
2023-04-05 18:18:36 +0300

    description

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
 import Theme from '../Theme';
 import { useSelector } from 'react-redux';
 import { selectTheme } from 'redux/theme/themeSelectors';
-import Container from "../components/Container/Container";
+import Container from '../components/Container/Container';
+import RecipePage from '../pages/RecipePage';
 
 export const App = () => {
   const theme = useSelector(selectTheme);
-  return( <Theme themeValue={theme}>
-    <>
-    <Container/>
-    </>
-  </Theme>)
+
+  return (
+    <Theme themeValue={theme}>
+      <Container />
+      <Routes>
+        <Route path="/recipe/:recipeId" element={<RecipePage />} />
+      </Routes>
+    </Theme>
+  );
 };
--- src/components/Recipe/RecipeDescription.js
+++ src/components/Recipe/RecipeDescription.js
@@ -1,5 +1,26 @@
-const RecipeDescription = () => {
-    
-}
+import { ClockIcon } from './Clock';
+import {
+  Wrapper,
+  Title,
+  Description,
+  Button,
+  Time,
+} from './RecipeDescription.styled';
+
+const RecipeDescription = ({ title, description, time }) => {
+  return (
+    <Wrapper>
+      <Title>{title}</Title>
+      <Description>{description}</Description>
+      <Button type="button" aria-label="add to favorite">
+        Add to favorite recipes
+      </Button>
+      <Time>
+        <ClockIcon />
+        <span> {time} min</span>
+      </Time>
+    </Wrapper>
+  );
+};
 
 export default RecipeDescription;
