fa41506c3be0d699d45c0d99cb922d87bed9781e
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2023-04-05 15:13:20 +0300

    conflict

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/BondarViktoriia/so-yummy-front">
+        <BrowserRouter basename="/so-yummy-front">
           <App />
         </BrowserRouter>
       </PersistGate>
