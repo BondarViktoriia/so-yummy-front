3dd8f0fdc493cda12c49c1ec4ba192424520f102
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2023-04-05 16:00:21 +0300

    route

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/BondarViktoriia/so-yummy-front">
+        <BrowserRouter >
           <App />
         </BrowserRouter>
       </PersistGate>
