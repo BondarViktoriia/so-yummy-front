1886c217e810289fc404b0aefad54709e5eb2647
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2023-04-06 15:25:28 +0300

    add api function favorite

--- src/services/api/apiRecipe.js
+++ src/services/api/apiRecipe.js
@@ -24,10 +24,37 @@ export const getRecipeByCategory = async category => {
 
 export const getMainPage = async () => {
   try {
-    const { data } = await axios.get(`/recipes/main`);
+    const { data } = await axios.get('/recipes/main');
     return data;
   } catch (error) {
     console.log(error.message);
     return null;
   }
 };
+
+export const getFavorite = async () => {
+  try {
+    const response = await axios.get('/favorite');
+    return response.data;
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
+export const addToFavorite = async(id) => {
+  try {
+    const response = await axios.put('/favorite/', { id });
+    return response.data;
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
+export const deleteFromFavorite = async (id) => {
+  try {
+        const response = await axios.patch(`/favotite/${id}`);
+    return response.data;
+  } catch (error) {
+      console.log(error.message)
+  }
+}
