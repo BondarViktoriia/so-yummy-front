851a09439de88826131446ac3bcab2118a5daf05
Kate-Kitkat19 <katekateee1911@gmail.com>
2023-04-09 20:19:13 +0300

    Update ThemeToggler.js

--- src/components/ThemeToggler/ThemeToggler.js
+++ src/components/ThemeToggler/ThemeToggler.js
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { toggleTheme } from 'redux/theme/themeSlice';
 import { selectTheme } from 'redux/theme/themeSelectors';
@@ -7,7 +6,6 @@ import { Ball, Checkbox, Label, Switcher } from './ThemeToggler.styled';
 export const ThemeToggler = () => {
   const selectedMode = useSelector(selectTheme);
   const dispatch = useDispatch();
-  // const [mode, setMode] = useState(selectedMode.mode ?? 'light');
   const changeTheme = () => {
     dispatch(toggleTheme());
   };
