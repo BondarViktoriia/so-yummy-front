a5cc12172f33f06b3b94c50be59088cc2ff1683c
lady-araminta <jparkhomenko@gmail.com>
2023-04-09 23:18:43 +0300

    title

--- src/components/SharedLayout/SharedLayout.js
+++ src/components/SharedLayout/SharedLayout.js
@@ -1,25 +1,21 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from '../Header/Header';
 import { Footer } from '../Footer/Footer';
-import {
-  MainBox,
-  Box,
-  // SharedLayoutContainer,
-  // SharedLayoutBg,
-} from './SharedLayout.styled';
+import Container from '../Container/Container';
+import Title from '../Title/Title';
+import { MainBox, Box } from './SharedLayout.styled';
 
 export const SharedLayout = () => {
   return (
     <Box>
-      {/* <SharedLayoutContainer> */}
-      {/* <SharedLayoutBg> */}
       <Header />
+      <Container>
+        <Title>Title</Title>
+      </Container>
       <MainBox>
         <Outlet />
       </MainBox>
       <Footer />
-      {/* </SharedLayoutBg> */}
-      {/* </SharedLayoutContainer> */}
     </Box>
   );
 };
