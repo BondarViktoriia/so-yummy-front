34bb71b01451934fd74a7cbe1ffe7189350feb35
lady-araminta <jparkhomenko@gmail.com>
2023-04-10 12:24:26 +0300

    fix header

--- src/components/SharedLayout/SharedLayout.js
+++ src/components/SharedLayout/SharedLayout.js
@@ -1,17 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from '../Header/Header';
 import { Footer } from '../Footer/Footer';
-import Container from '../Container/Container';
-import Title from '../Title/Title';
 import { MainBox, Box } from './SharedLayout.styled';
 
 export const SharedLayout = () => {
   return (
     <Box>
       <Header />
-      <Container>
-        <Title>Title</Title>
-      </Container>
       <MainBox>
         <Outlet />
       </MainBox>
