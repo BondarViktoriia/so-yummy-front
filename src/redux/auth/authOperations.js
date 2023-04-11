2bfadcc1ed92f212b188df899d27e54610fc8db5
SlavaHlushko <slavahlushko90@gmail.com>
2023-04-11 20:04:03 +0300

    add route from signin to main

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+import { toast } from 'react-toastify';
 
 import { updateUserData } from '../../services/auth/auth';
 
@@ -80,6 +81,41 @@ export const updateUser = createAsyncThunk(
       return data;
     } catch (error) {
       return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const getCurrentUser = createAsyncThunk(
+  'auth/getCurrent',
+  async (_, thunkAPI) => {
+    // Reading the token from the state via getState()
+    const state = thunkAPI.getState();
+    const persistedToken = state.auth.token;
+
+    if (persistedToken === null) {
+      // If there is no token, exit without performing any request
+      return thunkAPI.rejectWithValue('Unable to download user information');
+    }
+    try {
+      // If there is a token, add it to the HTTP header and perform the request
+      setAuthToken(persistedToken);
+      const res = await axios.get('/auth/current');
+      return res.data;
+    } catch (error) {
+      // check is token is expired - and then delete it from local storage
+      if (error.response.data.message === 'jwt expired') {
+        // If the response status is 401, clear the auth header and purge the persisted data
+        clearAuthToken();
+        thunkAPI.dispatch({ type: 'persist/PURGE', key: 'persist:auth' });
+      }
+      toast.error(`${error.response.data.message}`, {
+        position: 'top-center',
+        autoClose: 2500,
+        closeOnClick: true,
+        pauseOnHover: true,
+        theme: 'colored',
+      });
+      return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
--- src/redux/auth/authSelectors.js
+++ src/redux/auth/authSelectors.js
@@ -5,3 +5,5 @@ export const selectUser = state => state.auth.user;
 export const selectIsRefreshing = state => state.auth.isRefreshing;
 
 export const selectLoginError = state => state.auth.loginError;
+
+export const selectToken = state => state.auth.token;
