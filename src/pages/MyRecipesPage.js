3e2a38e3685fe20e080b8b8a003531a24ab6486b
Roman <roman.baytala@gmail.com>
2023-04-11 19:24:22 +0300

    pagination-v2

--- src/pages/MyRecipesPage.js
+++ src/pages/MyRecipesPage.js
@@ -1,17 +1,59 @@
 import styled from 'styled-components';
 import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import {
+  // useSelector,
+  useDispatch,
+} from 'react-redux';
 import {
   getOwnRecipesOperation,
   deleteOwnRecipeOperation,
 } from 'redux/ownRecipe/ownRecipesOperation';
-import { getRecipes } from 'redux/ownRecipe/ownRecipesSelectors';
-import RecipesList from 'components/RecipeList/RecipesList';
+
+// import { getRecipes } from 'redux/ownRecipe/ownRecipesSelectors';
+// import RecipesList from 'components/RecipeList/RecipesList';
+
 import Title from 'components/Title/Title';
 import { ContainerStyled } from 'components/Container/Container.styled';
 
+import PaginationComp from 'components/Pagination/Pagination';
+
+const recipesTest = [
+  {
+    _id: '640cd5ac2d9fecf12e8897fc',
+    title: 'Spaghetti Bolognese',
+    description:
+      'An Italian pasta dish made with spaghetti and a meat-based sauce, typically containing beef, tomatoes, onions, garlic, and herbs.',
+    thumb: 'https://www.themealdb.com/images/media/meals/sutysw1468247559.jpg',
+    time: '45',
+  },
+  {
+    _id: '640cd5ac2d9fecf12e8897f0',
+    title: 'Teriyaki Chicken Casserole',
+    description:
+      'A Japanese-inspired casserole made with chicken, teriyaki sauce, rice, and vegetables.',
+    thumb: 'https://www.themealdb.com/images/media/meals/wvpsxx1468256321.jpg',
+    time: '75',
+  },
+  {
+    _id: '640cd5ac2d9fecf12e8897f5',
+    title: 'Mediterranean Pasta Salad',
+    description:
+      'A salad made with pasta, vegetables (such as tomatoes, cucumbers, and olives), feta cheese, and a dressing made with olive oil and lemon juice.',
+    thumb: 'https://www.themealdb.com/images/media/meals/wvqpwt1468339226.jpg',
+    time: '27',
+  },
+  {
+    _id: '640cd5ac2d9fecf12e8897f3',
+    title: 'Vegan Lasagna',
+    description:
+      'A plant-based version of the classic Italian dish, made with layers of pasta, tomato sauce, vegan cheese, and vegetables (such as spinach, zucchini, and mushrooms).',
+    thumb: 'https://www.themealdb.com/images/media/meals/rvxxuy1468312893.jpg',
+    time: '50',
+  },
+];
+
 const MyRecipesPage = () => {
-  const recipes = useSelector(getRecipes);
+  // const recipes = useSelector(getRecipes);
 
   const dispatch = useDispatch();
 
@@ -27,18 +69,20 @@ const MyRecipesPage = () => {
     <ContainerStyled>
       <Title>My recipes</Title>
       <div>
-        {recipes.recipes.length > 0 && (
-          <RecipesList
-            recipes={recipes.recipes}
-            page="my"
-            onDelete={removeOwnRecipe}
-          />
+        {recipesTest.length > 0 && (
+          <>
+            <PaginationComp
+              recipes={recipesTest}
+              page="my"
+              onDelete={removeOwnRecipe}
+              itemsPerPage={2}
+            />
+          </>
         )}
-        {recipes.recipes.length === 0 && (
+        {recipesTest.length === 0 && (
           <EmptyList>We are sorry, but You do not have own recipes.</EmptyList>
         )}
       </div>
-      {/* <RecipesList recipes={[]} page="my" /> */}
     </ContainerStyled>
   );
 };
