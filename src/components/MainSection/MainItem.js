0071cc5d65d06c139b3d18fce6962c099ffa49bc
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2023-04-12 21:14:02 +0300

    add location main page

--- src/components/MainSection/MainItem.js
+++ src/components/MainSection/MainItem.js
@@ -1,18 +1,19 @@
 import {MainLi, MainImage, MainImageTitle, MainImageText} from "./MainItem.styled"
+import { useLocation } from 'react-router-dom';
 
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 const MainItem = ({ meal }) => {
       const location = useLocation();
     return (<>
-        <MainLi key={meal._id}>
-             {/* <Link to={`/recipe/:id`} state={{ from: '/' }}> */}
+        <MainLi key={meal._id} >
+             <Link to={`/recipe/${meal._id}`} state={{ from: location }}>
             <MainImage src={meal.preview} alt="title" />
                 <MainImageTitle>
                     <MainImageText>{meal.title}</MainImageText>
                 </MainImageTitle>
-            {/* </Link> */}
+            </Link>
 
             </MainLi>
     </>
