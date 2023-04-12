8dd2f63e2c431d436eb01c7c56e81a9103a788d3
Yaroslava-Vorobets <slavavor85@gmail.com>
2023-04-12 19:36:18 +0200

    SearchForm

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -2,10 +2,7 @@ import {
   HeroSection,
   HeroTitle,
   HeroWrap,
-  HeroText,
-  Form,
-  Input, 
-  HeroButtont,
+  HeroText, 
   BgImgdesckTop,
   BgImgPlate,
   BgLeafdesckTop,
@@ -13,10 +10,10 @@ import {
 } from './HeroSection.styled';
 import Container from '../../components/Container/Container'
 import { HealsyBlockRec } from './HealsyBlockRec';
-import { Formik } from 'formik';
-// import { useNavigate } from 'react-router-dom';
+import { MainSearch } from "../MainSearch";
+
 export const SectionHero = () => {
-  // const navigate = useNavigate(); 
+
 
   return (
     <HeroSection>
@@ -33,13 +30,8 @@ export const SectionHero = () => {
               "What to cook?" is not only a recipe app, it is, in fact, your
               cookbook. You can add your own recipes to save them for the
               future.
-            </HeroText>
-              <Formik>
-              <Form>
-                <Input type="text" />
-                <HeroButtont type="submit">Search</HeroButtont>           
-              </Form>
-            </Formik>         
+          </HeroText>
+          <MainSearch/>        
             <HealsyBlockRec />       
         </HeroWrap>
       </Container>
--- src/components/MainSection/MainItem.js
+++ src/components/MainSection/MainItem.js
@@ -1,12 +1,15 @@
 import {MainLi, MainImage, MainImageTitle, MainImageText} from "./MainItem.styled"
+// import { Link } from 'react-router-dom';
 
 const MainItem = ({ meal }) => {
     return (<>
-       <MainLi key={meal._id}>
-                <MainImage src={meal.preview} alt="title" />
-            <MainImageTitle>
-                <MainImageText>{meal.title}</MainImageText>
-            </MainImageTitle>
+        <MainLi key={meal._id}>
+             {/* <Link to={`/recipe/:id`} state={{ from: '/' }}> */}
+            <MainImage src={meal.preview} alt="title" />
+                <MainImageTitle>
+                    <MainImageText>{meal.title}</MainImageText>
+                </MainImageTitle>
+            {/* </Link> */}
             </MainLi>
     </>
     )
