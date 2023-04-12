cbace31440f823e4376874f8b410781efa41a66d
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2023-04-12 13:20:15 +0300

    add navlink to main

--- src/components/MainSection/MainItem.js
+++ src/components/MainSection/MainItem.js
@@ -1,12 +1,16 @@
 import {MainLi, MainImage, MainImageTitle, MainImageText} from "./MainItem.styled"
+import { useLocation,NavLink } from 'react-router-dom';
 
 const MainItem = ({ meal }) => {
+      const location = useLocation();
     return (<>
-       <MainLi key={meal._id}>
+        <MainLi key={meal._id}>
+           <NavLink to={`/recipe/${meal._id}`} state={{ from: location }}>
                 <MainImage src={meal.preview} alt="title" />
             <MainImageTitle>
                 <MainImageText>{meal.title}</MainImageText>
-            </MainImageTitle>
+                </MainImageTitle>
+                </NavLink>
             </MainLi>
     </>
     )
