7f3c581e24241bd306f2326bf1ee333566441dd0
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2023-04-17 10:22:35 +0300

    resolve conflict

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,6 +1,6 @@
-
 import { SectionHero } from '../components/HeroSection/HeroSection'
 import MainSection from '../components/MainSection/MainSection'
+
 const MainPage = () => {
   return (
     <>
